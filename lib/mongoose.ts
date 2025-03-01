c2192264283cf8ce3299964e87dbfa2f8e049c44
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-03-01 02:10:54 +0100

    .

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,4 +1,32 @@
+// import mongoose from "mongoose";
+
+// let isConnected: boolean = false;
+
+// export const connectToDatabase = async () => {
+//   mongoose.set("strictQuery", true);
+
+//   if (!process.env.MONGODB_URL) {
+//     return console.log("Missing MONGODB_URL");
+//   }
+//   if (isConnected) {
+//     return console.log("MongoDB is already connected");
+//   }
+
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URL, {
+//       dbName: "tshirt-store",
+//     });
+//     isConnected = true;
+
+//     console.log("MongoDB is connected");
+//   } catch (error) {
+//     console.log("MongoDB connection failed", error);
+//   }
+// };
+
 import mongoose from "mongoose";
+
+export const runtime = "nodejs"; // 👈 Forsira Node.js runtime
 
 let isConnected: boolean = false;
 
@@ -6,20 +34,23 @@ export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
 
   if (!process.env.MONGODB_URL) {
-    return console.log("Missing MONGODB_URL");
+    console.log("Missing MONGODB_URL");
+    return;
   }
+
   if (isConnected) {
-    return console.log("MongoDB is already connected");
+    console.log("MongoDB is already connected");
+    return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       dbName: "tshirt-store",
     });
-    isConnected = true;
 
+    isConnected = true;
     console.log("MongoDB is connected");
   } catch (error) {
-    console.log("MongoDB connection failed", error);
+    console.error("MongoDB connection failed", error);
   }
 };
