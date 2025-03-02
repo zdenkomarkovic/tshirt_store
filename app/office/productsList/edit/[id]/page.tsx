baa5a67dce213067131ac4def7890cb23559ecf8
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-03-02 02:03:23 +0100

    .

--- app/office/productsList/edit/[id]/page.tsx
+++ app/office/productsList/edit/[id]/page.tsx
@@ -1,10 +1,14 @@
 import ProductForm from "@/components/forms/ProductForm";
+import { getCategories } from "@/lib/actions/category.action";
 import { getProductById } from "@/lib/actions/product.action";
 import { ParamsProps } from "@/lib/actions/sharedTypes";
+import { getTags } from "@/lib/actions/tag.action";
 import React from "react";
 
 const page = async ({ params }: ParamsProps) => {
   const result = await getProductById({ productId: params.id });
+  const categories = await getCategories();
+  const savedTags = await getTags();
 
   return (
     <>
@@ -13,7 +17,12 @@ const page = async ({ params }: ParamsProps) => {
         Edit Product
       </h2>
       <div>
-        <ProductForm type="Edit" productDetails={JSON.stringify(result)} />
+        <ProductForm
+          type="Edit"
+          productDetails={JSON.stringify(result)}
+          categories={JSON.stringify(categories)}
+          savedTags={JSON.stringify(savedTags)}
+        />
       </div>
     </>
   );
