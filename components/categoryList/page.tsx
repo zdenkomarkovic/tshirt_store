353f552e50903e4ddbe828c34c1f4ae454f88b5d
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-03-02 02:32:49 +0100

    .

--- components/categoryList/page.tsx
+++ components/categoryList/page.tsx
@@ -5,7 +5,7 @@ import EditDeleteAction from "../shared/EditDeleteAction";
 import { Switch } from "../ui/switch";
 
 const CategoryList = async () => {
-  const result = await getCategories();
+  const result = (await getCategories()) || [];
 
   return (
     <div>
