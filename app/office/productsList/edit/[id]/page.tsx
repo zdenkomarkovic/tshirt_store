0771a4730c38fe53a839b7319e9e3904a6d3208e
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-03-02 23:31:20 +0100

    .

--- app/office/productsList/edit/[id]/page.tsx
+++ app/office/productsList/edit/[id]/page.tsx
@@ -6,22 +6,24 @@ import { getTags } from "@/lib/actions/tag.action";
 import React from "react";
 
 const page = async ({ params }: ParamsProps) => {
+  // Pozivanje asinhronih funkcija
   const result = await getProductById({ productId: params.id });
-  const categories = await getCategories();
-  const savedTags = await getTags();
+  const categories = await getCategories(); // Osiguravanje da je pozvano asinhrono
+  const savedTags = await getTags(); // Osiguravanje da je pozvano asinhrono
+
+  // Definisanje sigurnih vrednosti u slučaju da je rezultat undefined
   const safeCategories = categories || [];
   const safeSavedTags = savedTags || [];
 
   return (
     <>
-      {" "}
       <h2 className="h2-bold my-10 text-center text-gray-900">Edit Product</h2>
       <div>
         <ProductForm
           type="Edit"
           productDetails={result}
           categories={JSON.stringify(safeCategories)}  {/* Convert to string */}
-          savedTags={JSON.stringify(safeSavedTags)} 
+          savedTags={JSON.stringify(safeSavedTags)}  {/* Convert to string */}
         />
       </div>
     </>
